54ddcf9892f377c0df9e54049c2bab419b0fdb30
hmrjtfhut <tw745723@gmail.com>
2024-06-11 16:44:13 -0400

    Update game.js

--- game.js
+++ game.js
@@ -12,8 +12,11 @@ document.addEventListener('DOMContentLoaded', () => {
             gameUrl = 'https://artclass.site/service/hvtrs8%2F-aqsgtq.1kj0%2Cngt-a%2Fdcnae%2Fod-dipe%2Fald%2Fiae-ildgx%2Chvmn';
             break;
         default:
-            gameUrl = 'https://artclass.site/service/hvtrs8%2F-aqsgtq.1kj0%2Cngt-224%3A%2Fknfez.jtol';
+            gameUrl = 'about:blank';
     }
 
-    gameFrame.src = gameUrl;
+    if (gameUrl !== 'about:blank') {
+        gameFrame.style.display = 'block';
+        gameFrame.src = gameUrl;
+    }
 });
